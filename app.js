92504b4695dad94a937b0ae2a54827289b867afb
tomoyat <20t.toya@gmail.com>
2014-01-09 23:56:28 +0900

    added top page

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var express = require('express');
 var routes = require('./routes');
 var signup = require('./routes/signup');
 var signin = require('./routes/signin');
+var top = require('./routes/top');
 var http = require('http');
 var path = require('path');
 
@@ -34,6 +35,7 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/signup', signup.signup);
 app.get('/signin', signin.signin);
+app.get('/top', top.top);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
---
+++ routes/top.js
@@ -0,0 +1,8 @@
+
+/*
+ * GET home page.
+ */
+
+exports.top = function(req, res){
+  res.render('top');
+};
